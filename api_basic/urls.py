4ddec0c5a51b0d7511470878297382f75bf57446
RohitChandanshiv <chandanshivrb34@gmail.com>
2021-05-06 00:42:36 +0530

    api_basic urls and view updated

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from .views import article_list
+from .views import article_list, article_details
 
 urlpatterns = [
     path('article', article_list),
+    path('article/<int:pk>', article_details),
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Article
@@ -19,5 +17,29 @@ def article_list(request):
         serializer = ArticleSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,status=201)
-        return JsonResponse(serializer.errors,status=400)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def article_details(request, **kwargs):
+    pk = kwargs['pk']
+    try:
+        article_detail = Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = ArticleSerializer(article_detail, many=False)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article_detail, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        article_detail.delete()
+        return HttpResponse("article" + article_detail.title + "is deleted", status=204)
